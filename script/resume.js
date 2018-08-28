b6cc733b5b0cccc9233524955635d79ce065c8e3
OdysLam <odysseas007@windowslive.com>
2018-08-28 09:34:14 +0300

    Refactored site, removed unecessery info Added more project in portofolio

--- script/resume.js
+++ script/resume.js
@@ -14,17 +14,24 @@ $(document).keyup(
 
                 switch (tmp) {
                     case 'help':
-                        result = result + '<br>	<span style="cursor:pointer" class = "nav_button">help</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show a list of available commands<br>\
-										<span style="cursor:pointer" class = "nav_button">contact</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show contact information<br>\
-										<span style="cursor:pointer" class = "nav_button">skills</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show a list available skills.<br>\
-										<span style="cursor:pointer" class = "nav_button">education</span>  &nbsp&nbsp&nbsp&nbsp Show a list of available Degrees<br>\
-										<span style="cursor:pointer" class = "nav_button">info</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show general info about me<br>\
-										<span style="cursor:pointer" class = "nav_button">experience</span> &nbsp&nbsp&nbsp Show a list of available experience<br>\
-										<span style="cursor:pointer" class = "nav_button">clear</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Clear the console<br>\
-										<span style="cursor:pointer" class = "nav_button">social</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show a list of available social media<br>\
-										<span style="cursor:pointer" class = "nav_button">projects</span> &nbsp&nbsp&nbsp&nbsp&nbsp Show a list of the available past projects<br>\
-                                        <span style="cursor:pointer" class = "nav_button">blog</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Navigate my personal technology blog<br>\
-										<span style="cursor:pointer" class = "nav_button">about</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Show info about this site ';
+                        result = result + ' &nbsp <br>\
+                                        &nbsp <br>\
+                                        <span style="cursor:pointer" class = "nav_button">help</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp List of available commands<br>\
+										&nbsp <br>\
+										<span style="cursor:pointer" class = "nav_button">info</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Odysseas Resumé<br>\
+										&nbsp <br>\
+										<span style="cursor:pointer" class = "nav_button">contact</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Contact information<br>\
+										&nbsp <br>\
+										<span style="cursor:pointer" class = "nav_button">social media</span> &nbsp List of available social media<br>\
+                                        &nbsp <br>\
+										<span style="cursor:pointer" class = "nav_button">projects</span> &nbsp&nbsp&nbsp&nbsp&nbsp Odysseas portofolio <br>\
+                                        &nbsp <br>\
+                                        <span style="cursor:pointer" class = "nav_button">blog</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Navigate the blog<br>\
+                                        &nbsp <br>\
+                                        <span style="cursor:pointer" class = "nav_button">about</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Info about this site <br>\
+                                        &nbsp <br>\
+                                        <span style="cursor:pointer" class = "nav_button">clear</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Clear the console<br>\
+										 ';
 
                         break;
 
@@ -38,70 +45,51 @@ $(document).keyup(
 
                         break;
 
-                    case 'skills':
-                        result = result + '<br>	Languages:<br>\
-											&nbsp&nbspFrench:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspAdvanced (C1)<br>\
-											&nbsp&nbspEnglish:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspProficient (C2)<br>\
-											&nbsp&nbspGreek:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspNative <br>\
-										Techical Skills:<br>\
-											&nbsp&nbspPython:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#######&nbsp&nbsp&nbsp|70%<br>\
-											&nbsp&nbspC:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#####&nbsp&nbsp&nbsp&nbsp&nbsp|40%<br>\
-                                            &nbsp&nbspHTML&CSS:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#####&nbsp&nbsp&nbsp&nbsp&nbsp|50%<br>\
-                                            &nbsp&nbspJavascript:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|######&nbsp&nbsp&nbsp&nbsp|60%<br>\
-                                            &nbsp&nbspPhotoshop/Illustrator|#####&nbsp&nbsp&nbsp&nbsp&nbsp|40%<br>\
-                                            &nbsp&nbspJekyll&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#####&nbsp&nbsp&nbsp&nbsp&nbsp|50%<br>\
-                                            &nbsp&nbspAutodesk Eagle&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#####&nbsp&nbsp&nbsp&nbsp&nbsp|50%<br>\
-                                        Attributes:<br>\
-                                            &nbsp&nbspInitiative:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#########&nbsp|90%<br>\
-                                            &nbsp&nbspLeadership:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|######&nbsp&nbsp&nbsp&nbsp|70%<br>\
-                                            &nbsp&nbspManagement:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#######&nbsp&nbsp&nbsp|70%<br>\
-                                            &nbsp&nbspTeamwork:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#######&nbsp&nbsp&nbsp|70%<br>\
-                                            &nbsp&nbspCreativity:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#########&nbsp|90%<br>\
-                                            &nbsp&nbspCommunication:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp|#######&nbsp&nbsp&nbsp|70%<br>\
-                                            &nbsp&nbspLearning-Agility:&nbsp&nbsp&nbsp&nbsp|########&nbsp&nbsp|80%<br> ';
-                        break;
-
-                    case 'education':
-                        result = result + '<br>	2014 - Current: Electrical Engineering & Computer Science, University of Patras,Greece<br>\
-                                            &nbsp&nbsp GPA: 7.2/10<br>\
-                                            &nbsp&nbsp Thesis (2017 - Current): A distributed WASN based on an innovative blockchain <br>\
-                                            &nbsp&nbsp Executive officer at Mindspace Patras, the first Entrepreneurship club in UoP<br>\
-                                            2011-2014 High School Diploma, Experimental High School of Evangelic<br>\
-                                            &nbsp&nbsp Grade: 19.5 | Panhellenic examinations for university entry: 18.706/20.000<br>';
-                        break;
-
                     case 'info':
                         var birthday = +new Date('1996-05-15');
                         var age = ((Date.now() - birthday) / (31557600000));
                         result = result + '<br>	Surname:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspLamtzidis<br>\
 										    Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspOdyssefs(Odysseas)<br>\
-										    Profession:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspElectrical Engineering Student<br>\
 										    Age:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + age.toFixed() + '<br>\
 										    Nationality:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspGreek<br>\
 										    Driving permits:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspB<br>\
 										    Resumé:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="assets/cv.pdf" target = "_blank">pdf&larr;</a><br>';
                         break;
 
-                    case 'experience':
-                        result = result + '<br>April 2016 - September 2017<br>\
-											&nbsp&nbspSocial Media Manager, PatrasIQ<br>\
-												&nbsp&nbsp Managing the online brand/promotion of Patras IQ during, after the event and throughout the year <br>';
-                        break;
-
                     case 'clear':
                         result = '';
                         break;
 
-                    case 'social':
-                        result = result + '<br> <a href="https://gr.linkedin.com/in/lamtzidisodysseas" target = "_blank">LinkedIn &larr;</a><br>\
+                    case 'social media':
+                        result = result + '&nbsp <br>\
+                                        <br> <a href="https://gr.linkedin.com/in/lamtzidisodysseas" target = "_blank">LinkedIn &larr;</a><br>\
+                                        &nbsp <br>\
 										<a href="https://github.com/OdysLam" target = "_blank">GitHub &larr;</a><br>\
-										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">facebook &larr;</a><br>';
+										&nbsp <br>\
+										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">facebook &larr;</a><br>\
+										&nbsp <br>\
+										<a href="https://www.instagram.com/odysseytomit/" target = "_blank">Instagram &larr;</a><br>\
+										&nbsp <br>\
+										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">Odyssey to MIT facebook &larr;</a><br>';
                         break;
 
                     case 'projects':
-                        result = result + '<br>Glados: A DIY smart-home project | <a href = "/assets/glados_pres.pdf" target = "_blank">Presentation&larr;</a>, <a href ="/assets/glados_paper.pdf" target = "_blank">Paper&larr;</a>, <a href="https://github.com/OdysLam/GLaDOS-project" target = "_blank">Github&larr; |</a><br>\
-                                           This site | <a href = "https://github.com/OdysLam/odyslam.github.io" target = "_blank">Github&larr; |</a><br>\
-                                           Spotify local RESTful API | <a href = "https://github.com/OdysLam/spotify-local-http-api" target = "_blank">Github&larr; |</a><br>';
+                        result = result + '&nbsp <br>\
+                                       <br>Glados: A DIY smart-home project | <a href = "/assets/glados_pres.pdf" target = "_blank">Presentation&larr;</a>, <a href ="/assets/glados_paper.pdf" target = "_blank">Paper&larr;</a>, <a href="https://github.com/OdysLam/GLaDOS-project" target = "_blank">Github&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Odyslam.me | <a href = "https://github.com/OdysLam/odyslam.github.io" target = "_blank">Github&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Business Card | <a href = "/assets/biz_card.pdf" target = "_blank">Pdf&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Spotify local RESTful API | <a href = "https://github.com/OdysLam/spotify-local-http-api" target = "_blank">Github&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Mindspace | <a href = "https://www.mindspace.gr" target = "_blank">Mindspace&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Odyssey to MIT: A crowdfunding Campaign  | <a href = "https://www.giveandfund.com/giveandfund/project/odysseytomit" target = "_blank"> Site&larr;, <a href ="https://www.facebook.com/odysseytomit" target = "_blank">Facebook Page&larr;</a> |</a><br>\
+                                           &nbsp <br>\
+                                           Research | <a href = "https://www.google.gr" target = "_blank">TBA&larr; |</a><br>\
+                                           &nbsp <br>\
+                                           Pangea | <a href = "https://www.google.gr" target = "_blank">TBA&larr; |</a><br>';
 
                         break;
 
