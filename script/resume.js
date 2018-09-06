843f0fd4743ea2159fbc58b29f7d1514baafc01d
OdysLam <odysseas007@windowslive.com>
2018-09-06 09:02:55 +0300

    Minor
    New lighter pdf, added cv tracking bitly

--- script/resume.js
+++ script/resume.js
@@ -2,20 +2,20 @@ var root = 'ip@raspberry ~ $ ';
 var ip;
 
 $(document).keyup(
-        function(e) {
-            if (e.keyCode == 13) {
-                console.log("event triggered");
-                var result = $('#label').html();
+    function(e) {
+        if (e.keyCode == 13) {
+            console.log("event triggered");
+            var result = $('#label').html();
 
-                var tmp = $("#terminal").val().toLowerCase();
-                tmp = tmp.replace(/</g, "&lt;").replace(/>/g, "&gt;"); /* prevent XSS attacks */
-                var rootTemp = root + tmp;
-                $('#terminal').val('');
-                result = result + rootTemp;
+            var tmp = $("#terminal").val().toLowerCase();
+            tmp = tmp.replace(/</g, "&lt;").replace(/>/g, "&gt;"); /* prevent XSS attacks */
+            var rootTemp = root + tmp;
+            $('#terminal').val('');
+            result = result + rootTemp;
 
-                switch (tmp) {
-                    case 'help':
-                        result = result + ' &nbsp <br>\
+            switch (tmp) {
+                case 'help':
+                    result = result + ' &nbsp <br>\
                                         &nbsp <br>\
                                         <span style="cursor:pointer" class = "nav_button">help</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp List of available commands<br>\
 										&nbsp <br>\
@@ -36,35 +36,35 @@ $(document).keyup(
                                         <span style="cursor:pointer" class = "nav_button">Exit</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Exit this terminal<br>\
 										 ';
 
-                        break;
+                    break;
 
-                    case 'contact':
-                        result = result + '<br>	25 Martiou str 112,Patras,Greece<br>\ ' +
-                            'Tel: &nbsp&nbsp&nbsp(+30)6980950995<br>\
+                case 'contact':
+                    result = result + '<br>	25 Martiou str 112,Patras,Greece<br>\ ' +
+                        'Tel: &nbsp&nbsp&nbsp(+30)6980950995<br>\
                         Skype: &nbspodys_lamtzidis<br>\
                         Mail: <a href="mailto:hi@odyslam.me">&nbsp&nbsphi@odyslam.me</a><br> ';
 
 
 
-                        break;
+                    break;
 
-                    case 'info':
-                        var birthday = +new Date('1996-05-15');
-                        var age = ((Date.now() - birthday) / (31557600000));
-                        result = result + '<br>	Surname:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspLamtzidis<br>\
+                case 'info':
+                    var birthday = +new Date('1996-05-15');
+                    var age = ((Date.now() - birthday) / (31557600000));
+                    result = result + '<br>	Surname:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspLamtzidis<br>\
 										    Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspOdyssefs(Odysseas)<br>\
 										    Age:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + age.toFixed() + '<br>\
 										    Nationality:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspGreek<br>\
 										    Driving permits:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspB<br>\
-										    Resumé:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="assets/cv.pdf" target = "_blank">pdf&larr;</a><br>';
-                        break;
+										    Resumé:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="http://bit.ly/odyscv" target = "_blank">pdf&larr;</a><br>';
+                    break;
 
-                    case 'clear':
-                        result = '';
-                        break;
+                case 'clear':
+                    result = '';
+                    break;
 
-                    case 'social media':
-                        result = result + '&nbsp <br>\
+                case 'social media':
+                    result = result + '&nbsp <br>\
                                         <br> <a href="https://gr.linkedin.com/in/lamtzidisodysseas" target = "_blank">LinkedIn &larr;</a><br>\
                                         &nbsp <br>\
 										<a href="https://github.com/OdysLam" target = "_blank">GitHub &larr;</a><br>\
@@ -74,10 +74,10 @@ $(document).keyup(
 										<a href="https://www.instagram.com/odysseytomit/" target = "_blank">Instagram &larr;</a><br>\
 										&nbsp <br>\
 										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">Odyssey to MIT facebook &larr;</a><br>';
-                        break;
+                    break;
 
-                    case 'projects':
-                        result = result + '&nbsp <br>\
+                case 'projects':
+                    result = result + '&nbsp <br>\
                                        <br>Education & Research || Published Research, Tutorials, etc. , 2017-  |  <a href = "https://www.researchgate.net/profile/Odysseas_Lamtzidis" target = "_blank">ResearchGate&larr; |</a><br>\
                                            &nbsp <br>\
                                            &nbsp <br>\
@@ -105,45 +105,47 @@ $(document).keyup(
                                            Glados V1 || A DIY smart-home project, 2015-2017  | <a href = "/assets/glados_pres.pdf" target = "_blank">Presentation&larr;</a>, <a href="https://github.com/OdysLam/GLaDOS-project" target = "_blank">Github&larr; |</a><br>\
                                            &nbsp <br>\ ';
 
-                        break;
+                    break;
 
-                    case 'exit':
-                        window.open('https://www.google.gr','_self');
-                        break;
+                case 'exit':
+                    window.open('https://www.google.gr', '_self');
+                    break;
 
 
-                    case 'about':
-                        result = result + '<br>This site was made with &hearts; by me, based on terminal resume by Kevin Grillet<br>\
+                case 'about':
+                    result = result + '<br>This site was made with &hearts; by me, based on terminal resume by Kevin Grillet<br>\
                                            Typed.js is used,please leave a star @<a href="http://www.mattboldt.com/demos/typed-js/" target ="_blank">Mattboldt&larr;</a>';
-                        break;
-                    case 'blog':
-                        window.open('/blog/', '_self');
-                        break;
+                    break;
+                case 'blog':
+                    window.open('/blog/', '_self');
+                    break;
 
-                    case '':
-                        result = result;
-                        break;
-                    default:
-                        result = result + '<br> -bash: ' + tmp + ' is incorrect or forbidden for IP:<' + ip + '>, please type or click <span style="cursor:pointer" class = "nav_button">help</span> <br>';
-                        break;''
-                }
-
-                result = result + '<br>';
-                $('#label').html(result);
-                //needed to clear older binds
-                $('.nav_button').unbind();
-                $(".nav_button").click(function(){
-                    var value = $(this).text();
-                    $('#terminal').val(value);
-                    var e = jQuery.Event('keyup');
-                    e.keyCode = 13;
-                    $('.console').trigger(e);
-                });
-                $('html, body').animate({scrollTop: $("#terminal").offset().top
-                }, 1);
+                case '':
+                    result = result;
+                    break;
+                default:
+                    result = result + '<br> -bash: ' + tmp + ' is incorrect or forbidden for IP:<' + ip + '>, please type or click <span style="cursor:pointer" class = "nav_button">help</span> <br>';
+                    break;
+                    ''
             }
+
+            result = result + '<br>';
+            $('#label').html(result);
+            //needed to clear older binds
+            $('.nav_button').unbind();
+            $(".nav_button").click(function() {
+                var value = $(this).text();
+                $('#terminal').val(value);
+                var e = jQuery.Event('keyup');
+                e.keyCode = 13;
+                $('.console').trigger(e);
+            });
+            $('html, body').animate({
+                scrollTop: $("#terminal").offset().top
+            }, 1);
         }
-    )
+    }
+)
 $(document).ready(
     function() {
         $("#terminal").focus();
@@ -154,13 +156,13 @@ $(document).ready(
         });
         result = '<br> ';
         $(".updates").typed({
-            strings: ["hello visitor","to navigate, click or type the commands","type or click <span style=\"cursor:pointer\" class = \"nav_button\">help</span> for a Command List"],
+            strings: ["hello visitor", "to navigate, click or type the commands", "type or click <span style=\"cursor:pointer\" class = \"nav_button\">help</span> for a Command List"],
             typeSpeed: 25,
             backDelay: 500,
             showCursor: false,
-            callback : function(){
+            callback: function() {
                 console.log("typed completed")
-                $(".nav_button").click(function(){
+                $(".nav_button").click(function() {
                     var value = $(this).text();
                     console.log(value);
                     $('#terminal').val(value);
@@ -175,6 +177,6 @@ $(document).ready(
 
 
 
-    /**
-     * Created by Odys on 28/12/2016.
-     */
+/**
+ * Created by Odys on 28/12/2016.
+ */
