56c5821296712bea429e20725fb0339e9ce56f73
odyslam <odyslam@gmail.com>
2020-08-03 22:33:27 +0300

    fix starttech link 404
    Signed-off-by: odyslam <odyslam@gmail.com>

--- blog/test.sh
+++ blog/test.sh
@@ -1,5 +1,9 @@
-export DEV_UUID=b6811f2
-balena tunnel $DEV_UUID -p 2222:7459
-# sleep 10
-# CONTAINER_ID=$(ssh -Tp 7459 root@127.0.0.1 <<< 'balena-engine ps' | grep 'nginx' | awk '{print $1}')
-# ssh -Tp 22222 root@b6811f2.local "balena-engine exec $CONTAINER_ID /bin/sh /update-blog.sh"
+
+echo "Reading balena token from file.."
+token=$(cat balena_token)
+balena login -t $token
+balena tunnel b6811f2 -p 22222:1234 &
+echo "Sleeping for 6s to allow the tunnel to be established"
+sleep 6
+CONTAINER_ID=$(ssh -Tp 1234 root@127.0.0.1 <<< 'balena-engine ps' | grep 'nginx' | awk '{print $1}')
+ssh -Tp 1234 root@127.0.0.1 "balena-engine exec $CONTAINER_ID /bin/sh /update-blog.sh"
