93d1ad6b1001fc73997533df996c2a6432baf1b8
odyslam <odyslam@gmail.com>
2021-01-03 16:09:38 +0200

    Update menu

--- script/resume.js
+++ script/resume.js
@@ -54,9 +54,7 @@ $(document).keyup(
                                         &nbsp <br>\
                                         <span style="cursor:pointer" class = "nav_button">projects</span> &nbsp&nbsp&nbsp&nbsp&nbsp My portfolio <br>\
                                         &nbsp <br>\
-                                        <span style="cursor:pointer" class = "nav_button">cv</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Resumé <br>\
-                                        &nbsp <br>\
-										<span style="cursor:pointer" class = "nav_button">contact</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Get in touch & social media<br>\
+										<span style="cursor:pointer" class = "nav_button">contact</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Get in touch<br>\
                                         &nbsp <br>\
                                         <span style="cursor:pointer" class = "nav_button">blog</span> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMy personal blog, mainly about tech<br>\
                                         &nbsp <br>\
@@ -70,26 +68,9 @@ $(document).keyup(
 
                     case 'about':
                         result = result + '<br>	This website was made with <3 by Odysseas Lamtzidis  (ODYSLAM IKE P.C). <br>\
-                        Address1: Vasilissis Sofias 20, 17121. Nea Smyrni, Athens, Greece <br>\
-                        Address2: The Internet <br>\
-                        Mail: <a href="mailto:hi@odyslam.me">hi@odyslam.me</a><br>';
+                        Address: The Internet <br>\
+                        Mail: <a href="mailto:hi@odyslam.me">odyslam@gmail.com</a><br>';
                     break;
-
-                // case 'resume':
-                //     var birthday = +new Date('1996-05-15');
-                //     var age = ((Date.now() - birthday) / (31557600000));
-                //     result = result + '<br>	Surname:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspLamtzidis<br>\
-                // 					    Name:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspOdyssefs(Odysseas)<br>\
-                // 					    Age:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + age.toFixed() + '<br>\
-                // 					    Mail:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a href="mailto:hi@odyslam.me">hi@odyslam.me</a><br> \
-                // 					    Resumé:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<a class = "link" id="cv" href="assets/cv.pdf" target = "_blank">pdf&larr;</a><br>';
-                //     break;
-                case 'resume':
-                case 'resumé':
-                case 'cv':
-                    window.open('https://drive.google.com/file/d/1Ad030jSUuEujH-eXrscouJFlQ4EuMXZc/view?usp=sharing');
-                    break;
-
                 case 'clear':
                     result = '';
                     break;
@@ -101,50 +82,22 @@ $(document).keyup(
                                         &nbsp <br>\
 										<a href="https://github.com/OdysLam" target = "_blank">GitHub &larr;</a><br>\
 										&nbsp <br>\
-										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">Facebook &larr;</a><br>\
-										&nbsp <br>\
-										<a href="https://twitter.com/" target = "_blank">Twitter &larr;</a><br>\
-										&nbsp <br>\
-										<a href="https://www.facebook.com/odysseas.lamtzidis" target = "_blank">Odyssey to MIT facebook &larr;</a><br>';
-
+										<a href="https://twitter.com/odysseas_lam" target = "_blank">Twitter &larr;</a><br>\
+										&nbsp <br>'
                     break;
-
                 case 'projects':
                     result = result + '&nbsp <br>\
                                         &nbsp <br>\
-                                          <span class="project_name"> Diploma Thesis </span> ||  An IoT Edge-as-a-service (Eaas) Distributed Architecture & Reference Implementation, 2019 | <a href = "https://www.researchgate.net/publication/336564357_An_IoT_Edge-as-a-service_Eaas_Distributed_Architecture_Reference_Implementation" target = "_blank">Thesis&larr;</a>, <a href = "https://github.com/OdysLam/thesis-eaas" target = "_blank">Github&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> TedX talk </span> || Speaker: "I am not making a Project, I am making my Future", 2019 | <a href = "https://www.youtube.com/watch?v=yn1NsY7LVxM" target = "_blank">Youtube&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Tech portfolio</span> || Algorithms, Tools/Scripts, IoT projects, 2017- | <a href = "https://github.com/OdysLam/" target = "_blank">Github&larr;</a>|<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Research </span> || Blockchain, IoT, 2017-2019  |  <a href = "https://www.researchgate.net/profile/Odysseas_Lamtzidis" target = "_blank">ResearchGate&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Pangea </span> || 1st Prize MIT Bootcamp on I&E, 2018  | <a href = "https://drive.google.com/file/d/1pU9GHVVFZK68aqKAz8XOdzhSWiTlT76s/view?usp=sharing" target = "_blank">Pitch-Deck&larr;</a>, <a href = "http://bootcamp.mit.edu/entrepreneurship/" target = "_blank">Website&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Odyssey to MIT </span> || A Crowdfunding Campaign, 2018  | <a href = "https://www.giveandfund.com/giveandfund/project/odysseytomit" target = "_blank"> Website&larr;</a>, <a href ="https://www.facebook.com/odysseytomit" target = "_blank">Facebook Page&larr;</a> |</a><br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Design </span> || Business Card, Resumé in Illustrator, 2017-2018  | <a href = "https://drive.google.com/file/d/1RDQpPyf1z4UmsFrdnLf-f936VHfds-ha/view?usp=sharing" target = "_blank">Business Card&larr;</a>, <a href = "https://drive.google.com/file/d/1npfkuPQy-fsQYBuPj_dKJl22qTCMjxER/view?usp=sharing" target = "_blank">Resumé&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> odyslam.me </span> || Website design (plain html, css, js), 2016-  | <a href = "https://github.com/OdysLam/odyslam.github.io" target = "_blank">Github&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Mindspace </span> || Founded Patras Chapter, 2016  | <a href = "https://www.mindspace.gr" target = "_blank">Mindspace&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> PCB Schematics</span> || Custom Hardware for Glados, 2017  | <a href = "https://drive.google.com/file/d/1mzLk7mwVpM66uxjkNKU4dKOvCRD8OlQh/view?usp=sharing" target = "_blank">PCB schematics&larr;</a>, <a href="https://github.com/OdysLam/GLaDOS_v0.3/tree/master/esp8266" target = "_blank">Github&larr;</a> |<br>\
-                                           &nbsp <br>\
-                                           &nbsp <br>\
-                                           <span class="project_name"> Glados V2 </span> || A DIY smart-home project, 2015-2017  | <a href = "https://drive.google.com/file/d/1LHQ19l1VnQlL7C01QMgR19Y691zYDgHe/view?usp=sharing" target = "_blank">Presentation&larr;</a>, <a href="https://github.com/OdysLam/GLaDOS-project" target = "_blank">Github&larr;</a> |<br>\
-                                           &nbsp <br>';
-
+                                        <span class="project_name"> Writing </span> || blog.py, Twitter  | <a href = "https://odyslam.com/blog" target = "_blank">Personal blog&larr;</a>, <a href = "https://twitter.com/odysseas_lam" target = "_blank">Twitter&larr;</a> |<br>\
+                                        &nbsp <br>\
+                                        &nbsp <br>\
+                                        <span class="project_name">Tech projects</span> || DevRel, Open Source, Tools/Scripts, IoT projects, 2017- | <a href = "https://github.com/OdysLam/" target = "_blank">Github&larr;</a>|<br>\
+                                        &nbsp <br>\
+                                        &nbsp <br>\
+                                        <span class="project_name"> Research </span> || Blockchain, IoT, 2017-2019  |  <a href = "https://www.researchgate.net/profile/Odysseas_Lamtzidis" target = "_blank">ResearchGate&larr;</a> |<br>\
+                                        &nbsp <br>\
+                                        &nbsp <br>\
+                                        <span class="project_name"> Design </span> || Odyslam.com, Business Card, Resumé in Illustrator, 2017-2018  | <a href = "https://drive.google.com/file/d/1RDQpPyf1z4UmsFrdnLf-f936VHfds-ha/view?usp=sharing" target = "_blank">Business Card&larr;</a>, <a href = "https://drive.google.com/file/d/1npfkuPQy-fsQYBuPj_dKJl22qTCMjxER/view?usp=sharing" target = "_blank">Resumé&larr;</a> |<br>'
                     break;
                 // case 'exit':
                 //     window.open('https://www.google.gr', '_self');
@@ -249,7 +202,7 @@ $(document).ready(
             showCursor: false,
             callback: function () {
                 $(".updates").typed({
-                    strings: ["hello visitor, thank you for coming <br> \\ (•◡•) / " , "type help or tap on <span style=\"cursor:pointer\" class = \"nav_button\">help</span> for a command list"],
+                    strings: ["hello visitor, welcome <br> \\ (•◡•) / " , "type help or tap on <span style=\"cursor:pointer\" class = \"nav_button\">help</span> for a command list"],
                     typeSpeed: 1,
                     backDelay: 1000,
                     showCursor: false,
